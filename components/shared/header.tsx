9f7ac5df8366b12cb55fd3a709628be4e9039a75
JediKnightGIT <jediknightru@gmail.com>
2024-09-02 17:39:27 +0300

    add header, filter controls controls

---
+++ components/shared/header.tsx
@@ -0,0 +1,53 @@
+import { cn } from '@/lib/utils';
+import React from 'react';
+import { ArrowRight, ShoppingCart, User } from 'lucide-react';
+
+import { Container } from './Container';
+import Image from 'next/image';
+import { Button } from '../ui';
+
+interface Props {
+  className?: string;
+}
+
+export const Header: React.FC<Props> = ({ className }) => {
+  return (
+    <header className={cn('border border-b', className)}>
+      <Container className="flex items-center justify-between py-8">
+        {/* left part */}
+        <div className="flex items-center gap-4">
+          <Image
+            src="https://cdn-icons-png.flaticon.com/512/3081/3081559.png"
+            alt="logo"
+            width={35}
+            height={35}
+          />
+          <div>
+            <h1 className="text-2xl uppercase font-black">Next Shop</h1>
+            <p className="text-sm">вкусно без точки</p>
+          </div>
+        </div>
+
+        {/* Правая часть */}
+        <div className="flex items-center gap-3">
+          <Button variant="outline" className="flex items-center gap-1">
+            <User size={16} />
+            Войти
+          </Button>
+
+          <div>
+            <Button className="group relative">
+              <b>350 ₽</b>
+              <span className="h-full w-[1px] bg-white/30 mx-3"></span>
+              <div className="flex items-center gap-1 transition duration-300 group-hover:opacity-0">
+                <ShoppingCart className="size-4 relative" strokeWidth={2} />
+                <b>3</b>
+              </div>
+              <ArrowRight className="w-5 absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0" />
+            </Button>
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
+};
---
+++ components/shared/index.ts
@@ -0,0 +1,9 @@
+export { Container } from './Container';
+export { Header } from './Header';
+export { Title } from './Title';
+export { Categories } from './Categories';
+export { SortPopup } from './sort-popup';
+export { TopBar } from './top-bar';
+export { FilterCheckbox } from './filter-checkbox';
+export { Filters } from './filters';
+export { RangeSlider } from './range-slider';
